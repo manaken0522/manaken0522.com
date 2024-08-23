e72e2487c331a4ba800bc2da2317bc30cee7373d
manaken0522 <107743491+manaken0522@users.noreply.github.com>
2024-08-23 22:56:38 +0900

    OK

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>manaken0522</title>
+    <meta name="description" content="manaken0522.com" />
+    <style>
+        body {
+            background-image: url("/images/background_photo.jpg");
+            background-repeat: no-repeat;
+            background-position-x: center;
+            background-position-y: 25%;
+            backdrop-filter: blur(4px);
+        }
+
+        div.main {
+            color: white;
+            font-size: 24px;
+            text-shadow: black 1px 0 10px;
+        }
+
+        div.contacts > a > img {
+            height: 48px;
+        }
+    </style>
+</svelte:head>
+
+<div class="main">
+    <h1>Profile</h1>
+    <img style="width: 256px; border-radius: 50%;" src="/images/manaken0522.jpg" alt="icon">
+    <p>
+        Name: <span style="font-weight: bold;">manaken0522</span><br>
+        Country: <span style="font-weight: bold;">Japan</span><br>
+        Birthday: <span style="font-weight: bold;">2007/05/22</span>
+    </p>
+    <figure><embed src="https://wakatime.com/share/@manaken0522/4eb3441f-c331-463f-a4f4-bb791c26c590.svg" /></figure>
+    <div class="contacts">
+        <h2>Contacts</h2>
+        <a href="mailto:contact@manaken0522.com" target="_blank"><img src="/images/email.png" alt="youtube"></a>
+        <a href="https://www.youtube.com/@manaken0522" target="_blank"><img src="/images/youtube.png" alt="youtube"></a>
+        <a href="https://x.com/manaken0522" target="_blank"><img src="/images/x.png" alt="x"></a>
+        <a href="https://discord.gg/RMgmPRDbRQ" target="_blank"><img src="/images/discord.png" alt="discord"></a>
+        <a href="https://github.com/manaken0522" target="_blank"><img src="/images/github.png" alt="github"></a>
+        <a href="https://t.me/manaken0522" target="_blank"><img src="/images/telegram.png" alt="telegram"></a>
+        <a href="https://mastodon.social/@manaken0522" target="_blank"><img src="/images/mastodon.png" alt="mastodon"></a>
+        <a href="https://misskey.io/@manaken0522" target="_blank"><img src="/images/misskey.png" alt="misskey"></a>
+        <a href="https://bsky.app/profile/manaken0522.com" target="_blank"><img src="/images/bluesky.png" alt="bluesky"></a>
+    </div>
+</div>
