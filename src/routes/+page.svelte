2969dc7e19e2921c9aa883de04bc0fdde4705bdf
manaken0522 <107743491+manaken0522@users.noreply.github.com>
2024-08-25 01:25:12 +0900

    Add location where background_photo.jpg was taken

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,17 +30,21 @@
         Country: <span style="font-weight: bold;">Japan</span><br>
         Birthday: <span style="font-weight: bold;">2007/05/22</span>
     </p>
-    <figure><embed src="https://wakatime.com/share/@manaken0522/4eb3441f-c331-463f-a4f4-bb791c26c590.svg" /></figure>
     <div class="contacts">
         <h2>Contacts</h2>
-        <a href="mailto:contact@manaken0522.com" target="_blank"><img src="/images/email.png" alt="youtube"></a>
-        <a href="https://www.youtube.com/@manaken0522" target="_blank"><img src="/images/youtube.png" alt="youtube"></a>
-        <a href="https://x.com/manaken0522" target="_blank"><img src="/images/x.png" alt="x"></a>
-        <a href="https://discord.gg/RMgmPRDbRQ" target="_blank"><img src="/images/discord.png" alt="discord"></a>
-        <a href="https://github.com/manaken0522" target="_blank"><img src="/images/github.png" alt="github"></a>
-        <a href="https://t.me/manaken0522" target="_blank"><img src="/images/telegram.png" alt="telegram"></a>
-        <a href="https://mastodon.social/@manaken0522" target="_blank"><img src="/images/mastodon.png" alt="mastodon"></a>
-        <a href="https://misskey.io/@manaken0522" target="_blank"><img src="/images/misskey.png" alt="misskey"></a>
-        <a href="https://bsky.app/profile/manaken0522.com" target="_blank"><img src="/images/bluesky.png" alt="bluesky"></a>
+        <a href="mailto:contact@manaken0522.com" target="_blank"><img src="/images/email.png" alt="Email"></a>
+        <a href="https://www.youtube.com/@manaken0522" target="_blank"><img src="/images/youtube.png" alt="YouTube"></a>
+        <a href="https://x.com/manaken0522" target="_blank"><img src="/images/x.png" alt="X/Twitter"></a>
+        <a href="https://discord.gg/RMgmPRDbRQ" target="_blank"><img src="/images/discord.png" alt="Discord"></a>
+        <a href="https://github.com/manaken0522" target="_blank"><img src="/images/github.png" alt="GitHub"></a>
+        <a href="https://t.me/manaken0522" target="_blank"><img src="/images/telegram.png" alt="Telegram"></a>
+        <a href="https://mastodon.social/@manaken0522" target="_blank"><img src="/images/mastodon.png" alt="Mastodon"></a>
+        <a href="https://misskey.io/@manaken0522" target="_blank"><img src="/images/misskey.png" alt="Misskey"></a>
+        <a href="https://bsky.app/profile/manaken0522.com" target="_blank"><img src="/images/bluesky.png" alt="Bluesky"></a>
+    </div>
+    <div style="text-align: right;">
+        <a href="https://maps.app.goo.gl/sYDQzej61JEZaBzG8" target="_blank">
+            <img src="/images/pin_drop.png" alt="Pin Drop" style="width: 48px;"><br>
+        </a>
     </div>
 </div>
