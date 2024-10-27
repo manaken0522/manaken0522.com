33154a10e7decb2c983b537718e7a2e0e3cf601e
manaken0522 <107743491+manaken0522@users.noreply.github.com>
2024-10-27 17:13:35 +0900

    使ってないSNSのリンクを削除

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,7 +28,7 @@
 </svelte:head>
 
 <div class="main">
-    <h1>manaken0522</h1>
+    <h1>Profile</h1>
     <img style="width: 256px; border-radius: 50%;" src="/images/manaken0522.jpg" alt="icon">
     <div class="contacts">
         <h2>Contacts</h2>
@@ -38,9 +38,6 @@
         <a href="https://discord.gg/RMgmPRDbRQ" target="_blank"><img src="/images/discord.png" alt="Discord"></a>
         <a href="https://github.com/manaken0522" target="_blank"><img src="/images/github.png" alt="GitHub"></a>
         <a href="https://t.me/manaken0522" target="_blank"><img src="/images/telegram.png" alt="Telegram"></a>
-        <a href="https://mastodon.social/@manaken0522" target="_blank"><img src="/images/mastodon.png" alt="Mastodon"></a>
-        <a href="https://misskey.io/@manaken0522" target="_blank"><img src="/images/misskey.png" alt="Misskey"></a>
-        <a href="https://bsky.app/profile/manaken0522.com" target="_blank"><img src="/images/bluesky.png" alt="Bluesky"></a>
     </div>
     <div style="text-align: right;">
         <a href="https://maps.app.goo.gl/sYDQzej61JEZaBzG8" target="_blank">
