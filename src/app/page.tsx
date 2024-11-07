f82e62289ded68a022c8dc51d23c719ffb5505ee
manaken0522 <107743491+manaken0522@users.noreply.github.com>
2024-11-07 20:09:27 +0900

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,22 @@
-import Image from "next/image";
+import Image from 'next/image';
+import Link from 'next/link';
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+export default function Page() {
+    return (
+        <div style={{backgroundImage: "url('/images/background_photo.jpg')", backgroundRepeat: "no-repeat", backgroundPositionX: "center", backgroundPositionY: "25%"}}>
+            <div className="w-full h-full mt-32 backdrop-blur-sm">
+                <span className="text-4xl mx-auto">Profile</span>
+                <Image src="/images/manaken0522.jpg" alt='icon' width={256} height={256} />
+                <span className="text-3xl mx-auto">Contacts</span>
+                <div className='flex justify-evenly'>
+                    <Link href="mailto:manaken0522.0u767@slmail.me" target="_blank"><Image src="/images/email.png" alt="Email" width={48} height={48} /></Link>
+                    <Link href="https://www.youtube.com/@manaken0522" target="_blank"><Image src="/images/youtube.png" alt="YouTube" width={48} height={48} /></Link>
+                    <Link href="https://x.com/manaken0522" target="_blank"><Image src="/images/x.png" alt="X" width={48} height={48} /></Link>
+                    <Link href="https://discord.gg/RMgmPRDbRQ" target="_blank"><Image src="/images/discord.png" alt="Discord" width={48} height={48} /></Link>
+                    <Link href="https://github.com/manaken0522" target="_blank"><Image src="/images/github.png" alt="GitHub" width={48} height={48} /></Link>
+                    <Link href="https://t.me/manaken0522" target="_blank"><Image src="/images/telegram.png" alt="Telegram" width={48} height={48} /></Link>
+                </div>
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    )
 }
