1fa933cba9a4e3985a02c9d38bb500b5f7066827
manaken0522 <107743491+manaken0522@users.noreply.github.com>
2024-11-07 20:09:58 +0900

    a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,16 +6,11 @@ const SF_Pro = localFont({src: './fonts/sf-pro-text_regular.woff2'});
 
 export default function Layout({children}: {children: React.ReactNode}) {
     return (
-        <html lang="ja" className={SF_Pro.className}>
-            <head>
-                <meta charSet="UTF-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <link rel="icon" href="/favicon.ico" />
-                <GoogleAnalytics gaId="G-C680912EEE" />
-            </head>
-            <body>
+        <html lang="ja">
+            <body className={SF_Pro.className}>
                 {children}
             </body>
+            <GoogleAnalytics gaId="G-C680912EEE" />
         </html>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,32 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import { Metadata, Viewport } from 'next';
+
+export const metadata: Metadata = {
+    title: 'manaken0522',
+    description: 'のサイト',
+    openGraph: {
+        images: '/images/manaken0522.jpg',
+    },
+    icons: {
+        icon: '/favicon.ico'
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: 'manaken0522',
+        description: 'のサイト',
+        creator: '@manaken0522',
+        creatorId: '1764669515210891264',
+        images: ['/images/manaken0522.jpg']
+    }
+}
+
+export const viewport: Viewport = {
+    themeColor: 'skyblue',
+    width: 'device-width',
+    initialScale: 1
+}
 
 export default function Page() {
     return (
-        <div style={{backgroundImage: "url('/images/background_photo.jpg')", backgroundRepeat: "no-repeat", backgroundPositionX: "center", backgroundPositionY: "25%"}}>
-            <div className="w-full h-full mt-32 backdrop-blur-sm">
-                <span className="text-4xl mx-auto">Profile</span>
-                <Image src="/images/manaken0522.jpg" alt='icon' width={256} height={256} />
-                <span className="text-3xl mx-auto">Contacts</span>
-                <div className='flex justify-evenly'>
-                    <Link href="mailto:manaken0522.0u767@slmail.me" target="_blank"><Image src="/images/email.png" alt="Email" width={48} height={48} /></Link>
-                    <Link href="https://www.youtube.com/@manaken0522" target="_blank"><Image src="/images/youtube.png" alt="YouTube" width={48} height={48} /></Link>
-                    <Link href="https://x.com/manaken0522" target="_blank"><Image src="/images/x.png" alt="X" width={48} height={48} /></Link>
-                    <Link href="https://discord.gg/RMgmPRDbRQ" target="_blank"><Image src="/images/discord.png" alt="Discord" width={48} height={48} /></Link>
-                    <Link href="https://github.com/manaken0522" target="_blank"><Image src="/images/github.png" alt="GitHub" width={48} height={48} /></Link>
-                    <Link href="https://t.me/manaken0522" target="_blank"><Image src="/images/telegram.png" alt="Telegram" width={48} height={48} /></Link>
-                </div>
-            </div>
-        </div>
+        <span>作成中</span>
     )
 }
