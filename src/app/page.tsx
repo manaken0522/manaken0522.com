3cdf21a119445622ffc6c174a32c1b3570cdefed
manaken0522 <107743491+manaken0522@users.noreply.github.com>
2024-11-07 22:16:35 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,13 @@ export const metadata: Metadata = {
         icon: '/favicon.ico'
     },
     openGraph: {
-        images: '/images/manaken0522.jpg',
+        images: 'https://manaken0522.com/images/manaken0522.jpg', //よくない
     },
     twitter: {
         card: 'summary_large_image',
         title: 'manaken0522',
         description: 'manaken0522.com',
-        images: ['/images/manaken0522.jpg'],
+        images: ['https://manaken0522.com/images/manaken0522.jpg'], //よくない
         creator: '@manaken0522',
         creatorId: '1764669515210891264'
     },
