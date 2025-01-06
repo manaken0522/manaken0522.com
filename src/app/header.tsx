a1cf25880e8e56d2784f6449583724face906cda
manaken0522 <107743491+manaken0522@users.noreply.github.com>
2025-01-06 17:48:40 +0900

    responsive header

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -6,14 +6,14 @@ export default function Header() {
         <div className="bg-blue-500 text-white h-12 flex items-center justify-evenly">
             <div className="w-1/4">
                 <Link href="/" className="flex justify-center items-center">
-                    <Image src="/images/manaken0522.jpg" width={32} height={32} alt="icon" className="mr-2"></Image>
+                    <Image src="/images/manaken0522.jpg" width={32} height={32} alt="icon" className="mr-2 hidden sm:block"></Image>
                     <span className="text-xl">manaken0522</span>
                 </Link>
             </div>
             <div className="w-2/4 flex justify-evenly text-lg">
                 <Link href="/">Home</Link>
             </div>
-            <div className="w-1/4 text-center text-gray-200 text-base">
+            <div className="w-1/4 text-center text-gray-200 text-base hidden sm:block">
                 <Link href="https://github.com/manaken0522/manaken0522.com" target="_blank">Edit this site</Link>
             </div>
         </div>
