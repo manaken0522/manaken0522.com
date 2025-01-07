91153c0c3770855aaa75b58a117fe5f70bd03b26
manaken0522 <107743491+manaken0522@users.noreply.github.com>
2025-01-07 22:35:09 +0900

    add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { GoogleAnalytics } from '@next/third-parties/google';
 import Header from './header';
 import localFont from 'next/font/local';
 import { Viewport } from 'next';
+import Footer from './footer';
 
 const SF_Pro = localFont({src: './fonts/sf-pro-text_regular.woff2'});
 
@@ -18,6 +19,7 @@ export default function Layout({children}: {children: React.ReactNode}) {
             <body className={SF_Pro.className}>
                 <Header />
                 {children}
+                <Footer />
             </body>
             <GoogleAnalytics gaId="G-C680912EEE" />
         </html>
