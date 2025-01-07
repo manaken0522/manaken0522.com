8774454d68891dfcd7a5d815c86d2ef749924832
manaken0522 <107743491+manaken0522@users.noreply.github.com>
2025-01-07 23:34:20 +0900

    bug fix

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,14 +1,18 @@
-'use client'
-
+import { headers } from "next/headers";
 import Link from "next/link";
 
-export default function Footer() {
+export default async function Footer() {
     let mirror_link;
-    if (location.hostname.split('.').slice(-1)[0] == 'onion') {
-        mirror_link = "manaken0522.com";
-    }
-    else {
-        mirror_link = "manakeneftm57kz5673nu6mzzhvw2tjlh4ruhb43hufy7shtqgxztyyd.onion";
+    const hostname = (await headers()).get("X-Hostname");
+    console.log(hostname);
+
+    if (hostname != undefined) {
+        if (hostname.split('.').slice(-1)[0] == 'onion') {
+            mirror_link = "manaken0522.com";
+        }
+        else {
+            mirror_link = "manakeneftm57kz5673nu6mzzhvw2tjlh4ruhb43hufy7shtqgxztyyd.onion";
+        }
     }
     return (
         <div className="text-center bg-blue-500 text-white py-2">
