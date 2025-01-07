400f71bb910234602e76e3ddd39c66890b28734c
manaken0522 <107743491+manaken0522@users.noreply.github.com>
2025-01-07 22:55:05 +0900

    add onion/surface mirror links

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,5 +1,20 @@
-export default function Header() {
+'use client'
+
+import Link from "next/link";
+
+export default function Footer() {
+    let mirror_link;
+    if (location.hostname.split('.').slice(-1)[0] == 'onion') {
+        mirror_link = "manaken0522.com";
+    }
+    else {
+        mirror_link = "manakeneftm57kz5673nu6mzzhvw2tjlh4ruhb43hufy7shtqgxztyyd.onion";
+    }
     return (
-        <div></div>
+        <div className="text-center bg-blue-500 text-white py-2">
+            <span className="text-lg">
+                Mirror: <Link href={"https://"+mirror_link}>{mirror_link}</Link>
+            </span>
+        </div>
     )
 }
