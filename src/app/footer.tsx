0af457622b4eebf218ceb9149138da7418819a9a
manaken0522 <107743491+manaken0522@users.noreply.github.com>
2025-01-10 02:58:34 +0900

    Update footer.tsx

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -4,7 +4,7 @@ export default async function Footer() {
     return (
         <div className="text-center bg-blue-500 text-white py-2">
             <span className="text-lg">
-                Onion Mirror: <Link href={"https://manakeneftm57kz5673nu6mzzhvw2tjlh4ruhb43hufy7shtqgxztyyd.onion"}>manakeneftm57kz5673nu6mzzhvw2tjlh4ruhb43hufy7shtqgxztyyd.onion</Link>
+                Onion Mirror: <Link href={"http://manakeneftm57kz5673nu6mzzhvw2tjlh4ruhb43hufy7shtqgxztyyd.onion"}>manakeneftm57kz5673nu6mzzhvw2tjlh4ruhb43hufy7shtqgxztyyd.onion</Link>
             </span>
         </div>
     )
