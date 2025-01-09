d20a0ee20344afdc9949755d6d33a57981c386a7
manaken0522 <107743491+manaken0522@users.noreply.github.com>
2025-01-10 02:43:41 +0900

    bug fix

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,23 +1,10 @@
-import { headers } from "next/headers";
 import Link from "next/link";
 
 export default async function Footer() {
-    let mirror_link;
-    const hostname = (await headers()).get("X-Hostname");
-    console.log(hostname);
-
-    if (hostname != undefined) {
-        if (hostname.split('.').slice(-1)[0] == 'onion') {
-            mirror_link = "manaken0522.com";
-        }
-        else {
-            mirror_link = "manakeneftm57kz5673nu6mzzhvw2tjlh4ruhb43hufy7shtqgxztyyd.onion";
-        }
-    }
     return (
         <div className="text-center bg-blue-500 text-white py-2">
             <span className="text-lg">
-                Mirror: <Link href={"https://"+mirror_link}>{mirror_link}</Link>
+                Onion Mirror: <Link href={"https://manakeneftm57kz5673nu6mzzhvw2tjlh4ruhb43hufy7shtqgxztyyd.onion"}>manakeneftm57kz5673nu6mzzhvw2tjlh4ruhb43hufy7shtqgxztyyd.onion</Link>
             </span>
         </div>
     )
