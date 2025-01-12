f653e18dd8bea16be22266ffb523ab631157cace
manaken0522 <107743491+manaken0522@users.noreply.github.com>
2025-01-12 23:54:35 +0900

    change footer

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -2,10 +2,9 @@ import Link from "next/link";
 
 export default async function Footer() {
     return (
-        <div className="text-center bg-blue-500 text-white py-2">
-            <span className="text-lg">
-                Onion Mirror: <Link href={"http://manakeneftm57kz5673nu6mzzhvw2tjlh4ruhb43hufy7shtqgxztyyd.onion"}>manakeneftm57kz5673nu6mzzhvw2tjlh4ruhb43hufy7shtqgxztyyd.onion</Link>
-            </span>
+        <div className="text-center bg-blue-500 text-white text-lg py-2">
+            <Link className="text-gray-200" href={"http://manakeneftm57kz5673nu6mzzhvw2tjlh4ruhb43hufy7shtqgxztyyd.onion"}>Onion Mirror</Link><br/>
+            Copyright 2025 manaken0522 All rights reserved.
         </div>
     )
 }
