fd9fc1e26999a232274a50cccdba98c139c0ed85
manaken0522 <107743491+manaken0522@users.noreply.github.com>
2025-12-02 04:13:31 +0900

    /tagsを直した

--- src/app/tags/page.tsx
+++ src/app/tags/page.tsx
@@ -1,6 +1,5 @@
 import { getAllPosts } from "@/lib/api";
 import Link from "next/link";
-import Container from "../_components/container";
 import Header from "../_components/header";
 
 export default function Tags() {
@@ -8,25 +7,20 @@ export default function Tags() {
 	const tags = posts.map((post) => post.tags).flat();
 
 	return (
-		<main>
-			<Container>
-				<Header />
-				<h2 className="mb-6 text-5xl font-bold tracking-tighter leading-tight">
-					Tags
-				</h2>
-				<ul>
-					{[...new Set(tags)].map((tag) => (
-						<li className="px-4 py-2 m-1 rounded-xl bg-white/8 inline-block">
-							<Link href={`/tags/${tag}`}>
-								{tag}
-								<div className="rounded-full bg-white/16 w-6 h-6 inline-block text-center ml-2">
-									{tags.filter((_tag) => _tag === tag).length}
-								</div>
-							</Link>
-						</li>
-					))}
-				</ul>
-			</Container>
-		</main>
+		<div>
+			<h2 className="mb-6 text-5xl font-bold text-center">Tags</h2>
+			<ul className="place-self-center">
+				{[...new Set(tags)].map((tag) => (
+					<li className="px-4 py-2 m-1 rounded-xl bg-white/8 inline-block">
+						<Link href={`/tags/${tag}`}>
+							{tag}
+							<div className="rounded-full bg-white/16 w-6 h-6 inline-block text-center ml-2">
+								{tags.filter((_tag) => _tag === tag).length}
+							</div>
+						</Link>
+					</li>
+				))}
+			</ul>
+		</div>
 	);
 }
